5e6475744b8a7bd801c913f5c9d347b2353e280b
Sakurai <K.Sakurai_2000@outlook.com>
2021-08-14 06:46:51 +0900

    add #1

--- output.py
+++ output.py
@@ -16,13 +16,12 @@ def main():
         print(list.id, list.name)
 
     id = input("choose id:")
-    #id = 885894452958470146
 
     f = open(screen_name + '.csv', 'w', encoding='UTF-8', newline="")
     writer = csv.writer(f)
 
+    writer.writerow(["内部ID", "名前", "ID", "フォロー数", "フォロワー数", "URL", "bio"])
     for member in tweepy.Cursor(api.list_members, list_id=id).items():
-        #内部ID, 表示名，ID
         #print(member)
         writer.writerow([member.id, member.name, member.screen_name, member.friends_count, member.followers_count, member.url, member.description])
 
