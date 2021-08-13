1c66d0c1662aef4b6a639245bcd60287ef303cd5
Sakurai <K.Sakurai_2000@outlook.com>
2021-08-13 20:08:15 +0900

    change #5

--- output.py
+++ output.py
@@ -2,22 +2,21 @@ import csv
 
 import tweepy
 
-import settings as st
+from twapi import generate_api
 
-auth = tweepy.OAuthHandler(st.CONSUMER_KEY, st.CONSUMER_SECRET)
-auth.set_access_token(st.ACCESS_TOKEN_KEY, st.ACCESS_TOKEN_SECRET)
-api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+api = generate_api()
 
 def main():
     #screen_name = input("screen_name:")
-    screen_name = '_r046'
+    screen_name = 'Sakurai_Absol'
 
     lists = api.lists_all(screen_name = screen_name)
     for list in lists:
         print(list.id, list.name)
 
-    #id = input("choose id:")
-    id = 885894452958470146
+    id = input("choose id:")
+    #id = 885894452958470146
 
     f = open(screen_name + '.csv', 'a', encoding='UTF-8', newline="")
     writer = csv.writer(f)
--- settings.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-load_dotenv()
-
-CONSUMER_KEY = os.environ.get("TW_KEY")
-CONSUMER_SECRET = os.environ.get("TW_KEY_SEC")
-ACCESS_TOKEN_KEY = os.environ.get("TW_ACC_TOKEN")
-ACCESS_TOKEN_SECRET = os.environ.get("TW_ACC_TOKEN_SEC")
-
