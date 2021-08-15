cbe3ec28c37aaade0e0ffb9495858be73553af3f
Sakurai <K.Sakurai_2000@outlook.com>
2021-08-15 16:11:39 +0900

    fix #2

--- output.py
+++ output.py
@@ -7,27 +7,36 @@ from twapi import generate_api
 
 api = generate_api()
 
+
+def get_list_id(screen_name):
+    lists = api.lists_all(screen_name = screen_name)
+    for list in lists:
+        print(list.id, list.name)
+    id = input("list id:")
+    return id
+
+
+def list_member_to_csv(list_id):
+    name = api.get_list(list_id = list_id).full_name.split('/')
+
+    file_name = '{}_{}.csv'.format(name[0], name[1])
+
+    f = open(file_name, "w+", encoding="UTF-8", newline="")
+    writer = csv.writer(f)
+
+    writer.writerow(["内部ID", "名前", "ID", "フォロー数", "フォロワー数", "URL", "bio"])
+    for member in tweepy.Cursor(api.list_members, list_id = list_id).items():
+        writer.writerow([member.id, member.name, member.screen_name, member.friends_count, member.followers_count, member.url, member.description])
+    f.close()
+
+
 def main():
     #screen_name = input("screen_name:")
     screen_name = 'Sakurai_Absol'
 
-    lists = api.lists_all(screen_name = screen_name)
-    for list in lists:
-        print(list.id, list.name)
+    list_id = get_list_id(screen_name)
 
-    id = input("choose id:")
-
-    list_name = api.get_list(list_id = id).name
-
-    f = open(screen_name + "_" + list_name + ".csv", "w+", encoding="UTF-8", newline="")
-    writer = csv.writer(f)
-
-    writer.writerow(["内部ID", "名前", "ID", "フォロー数", "フォロワー数", "URL", "bio"])
-    for member in tweepy.Cursor(api.list_members, list_id=id).items():
-        #print(member)
-        writer.writerow([member.id, member.name, member.screen_name, member.friends_count, member.followers_count, member.url, member.description])
-
-    f.close()
+    list_member_to_csv(list_id)
 
 
 if __name__ == '__main__':
